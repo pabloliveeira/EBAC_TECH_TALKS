15e2d7a1f9279bfc73e48037e9372de270dffaa4
Pablo Oliveira <pablo230197@gmail.com>
2024-05-15 10:17:38 -0300

    CRIANDO CONTAGEM

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,6 +584,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
+AOS.init();
+const dataDoEvento = new Date("Jan 29, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor(distanciaAteEvento % diaEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteEvento % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = `EVENTO EXPIRADO`;
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequireec61")
 
